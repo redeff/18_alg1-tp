809b6fc4f9a0fb4c6348d5e66b1a8a38a483c7ad
sscubino <57198297+sscubino@users.noreply.github.com>
2019-11-10 14:51:15 -0300

    modifique un poco permutaciones

--- Permutaciones.hs
+++ Permutaciones.hs
@@ -1,8 +1,8 @@
+
 shift :: Integer -> [Integer] -> [Integer]
 shift _ [] = []
-shift k (x:xs)
-	| x < k  =     x : shift k xs
-	| x >= k = (x+1) : shift k xs
+shift k (x:xs) | x < k  =     x : shift k xs
+               | x >= k = (x+1) : shift k xs
 
 poner :: Integer -> [Integer] -> [Integer]
 poner k xs = k : (shift k xs)
@@ -13,7 +13,16 @@ ponerCadaUna k (x:xs) = poner k x : ponerCadaUna k xs
 
 permutaciones :: Integer -> [[Integer]]
 permutaciones 0 = [[]]
+permutaciones n = annadir  n
+    where annadir :: Integer -> [[Integer]]
+          annadir 0 = []
+          annadir k = annadir (k-1) ++ ponerCadaUna k (permutaciones (n-1))
+
+{-
+permutaciones :: Integer -> [[Integer]]
+permutaciones 0 = [[]]
 permutaciones n = annadir n where
-	rec = permutaciones (n-1)
-	annadir 0 = []
-	annadir k = annadir (k-1) ++ ponerCadaUna k rec
+    rec = permutaciones (n-1)
+    annadir 0 = []
+    annadir k = annadir (k-1) ++ ponerCadaUna k rec
+-}
