dc4f17b8fecb0af924a6f60bafa64b9230342342
sscubino <57198297+sscubino@users.noreply.github.com>
2019-11-13 21:12:08 -0300

    Agregue TrabajoCompleto. queda mejorar esPrimo y comentar el tp

---
+++ TrabajoCompleto.hs
@@ -0,0 +1,77 @@
+type Circulo = [Integer]
+
+ordenar :: Circulo -> Circulo
+ordenar [] = []
+ordenar (x:xs) | x==1 = (x:xs)
+               | otherwise = ordenar (xs++[x])
+
+sonCirculosIguales :: Circulo -> Circulo -> Bool
+sonCirculosIguales a b = ordenar a == ordenar b
+
+esListaPrima :: [Integer] -> Bool
+esListaPrima xs | length xs == 0 = True
+                | length (tail xs) == 0 = True
+                | otherwise = esPrimo (head xs + head (tail xs)) && esListaPrima (tail xs)
+
+esCirculoPrimo :: Circulo -> Bool
+esCirculoPrimo [] = True
+esCirculoPrimo xs = esPrimo (head xs + last xs) && esListaPrima xs
+
+--Charles, se te ocurre una mejor funcion para esta??
+esPrimo :: Integer -> Bool
+esPrimo n = mod (fact (n-1)) n == (n-1)
+
+   where fact :: Integer -> Integer
+         fact 0 = 1
+         fact n = n * fact (n-1)
+
+--PERMUTACIONES:
+shift :: Integer -> [Integer] -> [Integer]
+shift _ [] = []
+shift k (x:xs) | x < k  =     x : shift k xs
+               | x >= k = (x+1) : shift k xs
+
+poner :: Integer -> [Integer] -> [Integer]
+poner k xs = k : (shift k xs)
+
+ponerCadaUna :: Integer -> [[Integer]] -> [[Integer]]
+ponerCadaUna _ [] = []
+ponerCadaUna k (x:xs) = poner k x : ponerCadaUna k xs
+
+permutaciones :: Integer -> [[Integer]]
+permutaciones 0 = [[]]
+permutaciones n = annadir  n
+    where annadir :: Integer -> [[Integer]]
+          annadir 0 = []
+          annadir k = annadir (k-1) ++ ponerCadaUna k (permutaciones (n-1))
+
+
+
+estaRepetidoPrimero :: [Circulo] -> Bool
+estaRepetidoPrimero [] = False
+estaRepetidoPrimero (x:xs) = pertenece x xs
+
+pertenece :: Circulo -> [Circulo] -> Bool
+pertenece _ [] = False
+pertenece a (x:xs) | sonCirculosIguales a x == True = True
+                   | otherwise = pertenece a xs 
+
+eliminarNoPrimos :: [Circulo] -> [Circulo]
+eliminarNoPrimos [] = []
+eliminarNoPrimos (x:xs) | esCirculoPrimo x = x : eliminarNoPrimos xs
+                        | otherwise = eliminarNoPrimos xs
+
+filtrarRepetidos :: [Circulo] -> [Circulo]
+filtrarRepetidos [] = []
+filtrarRepetidos (x:xs) | estaRepetidoPrimero (x:xs) == True = filtrarRepetidos xs
+                        | otherwise = x : filtrarRepetidos xs
+
+listaCirculosPrimos :: Integer -> [Circulo]
+listaCirculosPrimos n = filtrarRepetidos (eliminarNoPrimos (permutaciones n))
+
+
+contarCirculosPrimos :: Integer -> Integer
+contarCirculosPrimos n = long (listaCirculosPrimos n)
+    where long :: [Circulo] -> Integer
+          long [] = 0
+          long (x:xs) = 1 + long xs
